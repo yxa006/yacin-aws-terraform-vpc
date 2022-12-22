76d82f51fcc5ec71d28c1d28b565219d936e207e
yxa006 <yacin.bakir@gmail.com>
2022-12-22 16:08:25 -0500

    Add the content for the repository

---
+++ terraform/stage/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "1.0.9"
+
+  backend "s3" {
+    bucket         = "tfstate-289d53ee-aadb-82b7-3c1b-f6ff95aaef2c"
+    key            = "stage.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.57.0"
+    }
+  }
+}
---
+++ terraform/stage/variables.tf
@@ -0,0 +1,4 @@
+variable "allowed_account_id" {
+  description = "The id of the one AWS account this code is permitted to run against"
+  type        = string
+}
